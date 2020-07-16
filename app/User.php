ed1dd6e0be2aea986ae1e02abca30fb8c0e94d5a
ystmrdk <rattyasta1@gmail.com>
2020-07-16 23:43:30 +0700

    moving dir

--- app/User.php
+++ app/User.php
@@ -1,22 +1,22 @@
 <?php
 
-namespace App\Models;
+namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use Notifiable;
-
+    use Notifiable, HasRoles;
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'nama', 'email', 'password', 'nim', 'prodi', 'tlp'
+        'name', 'email', 'password'
     ];
 
     /**
