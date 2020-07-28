ea6cc1e3b92254324f8c58a76504836e9629369d
yastamardika <rattyasta1@gmail.com>
2020-07-28 17:27:32 +0700

    added controller for akademik

--- routes/web.php
+++ routes/web.php
@@ -49,8 +49,8 @@ Route::group(['middleware' => 'auth'], function () {
 
     //route group untuk petugas akademik
     Route::group(['middleware' => ['role:akademik']], function () {
-        Route::get('/dashboard/akademik', 'SidangRegController@index')->name('akademik.mahasiswa');
-        Route::post('/dashboard/akademik/{id}', 'SidangRegController@ajukan')->name('akademik.ajukan');
+        Route::get('/dashboard/akademik', 'HistoryController@index')->name('akademik.mahasiswa');
+        Route::post('/dashboard/akademik/{id}', 'HistoryController@ajukan')->name('akademik.ajukan');
     });
     //route yang berada dalam group ini, hanya bisa diakses oleh user
     //yang memiliki permission yang telah disebutkan dibawah
