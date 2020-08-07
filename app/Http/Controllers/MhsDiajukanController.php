afbe3725191c79d42aeff535a126494bd5b9f220
yastamardika <rattyasta1@gmail.com>
2020-08-08 02:31:00 +0700

    kaprodi_features

--- app/Http/Controllers/MhsDiajukanController.php
+++ app/Http/Controllers/MhsDiajukanController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Kaprodi;
 use App\Mahasiswa;
 use App\Penguji;
+use App\Sidang;
 use App\User;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
@@ -20,15 +21,30 @@ class MhsDiajukanController extends Controller
     }
 
     function lihatPenguji($id){
-        $role = Role::all()->where('name','penguji');
+        $prodi = Mahasiswa::findOrFail($id)->where('user_id', $id)->pluck('id_prodi');
+        $penguji = Penguji::all()->where('id_prodi',$prodi);
         $mhs = Mahasiswa::findOrFail($id);
 
-        return view('pages.kaprodi.penguji', $role);
+        return view('pages.kaprodi.penguji', $penguji);
     }
 
-    function pilihPenguji(Request $request){
+    function pilihPenguji(Request $request,$id){
 
-        $penguji = Penguji::all()->where('',);
-         return redirect()->back();
+        $sidang = Sidang::create([
+          'id_mhs'=> $id,
+          'id_penguji1'=> $request->penguji1,
+          'id_penguji2'=> $request->penguji2,
+          'id_pembimbing'=> $request->pembimbing,
+          'tanggal_sidang'=>$request->tanggal,
+        ]);
+        $sidang->save();
+
+
+        $mhs=Mahasiswa::findOrFail($id);
+        $mhs->toQuery()->update([
+            'id_status' => 2,
+        ]);
+
+        return redirect()->back();
     }
 }
