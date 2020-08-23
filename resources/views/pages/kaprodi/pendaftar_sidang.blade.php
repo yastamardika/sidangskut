3235e1affb1e1ce5d35dee0cbea2ef3bb17bf7bd
bagasnur <bagasnur00@mail.ugm.ac.id>
2020-08-23 18:47:07 +0700

    Kaprodi - tabel mahasiswa sidang & detail update

--- resources/views/pages/kaprodi/pendaftar_sidang.blade.php
+++ resources/views/pages/kaprodi/pendaftar_sidang.blade.php
@@ -1,7 +1,7 @@
 @component('layouts.template')
 
     @slot('title_page')
-        Sidang Prodi {{ $namaprodi->prodi_full }}
+        Sidang Prodi {{ $namaprodi->program_studi }}
     @endslot
     @slot('icon')
         bxs-spreadsheet
@@ -19,15 +19,14 @@
                     <h6><i>Belum ada data pengajuan sidang mahasiswa.</i></h6>
                 </div>
             @else
-                <h6 class="card-text">Unduh semua data pengajuan: </h6>
-                <button type="submit" class="btn btn-outline-dark mb-4"><i class='bx bxs-cloud-download bx-xs d-inline-flex pr-2 align-middle'></i><span class="align-middle">Unduh file</span></button>
+                {{-- <h6 class="card-text">Unduh semua data pengajuan: </h6>
+                <button type="submit" class="btn btn-outline-dark mb-4"><i class='bx bxs-cloud-download bx-xs d-inline-flex pr-2 align-middle'></i><span class="align-middle">Unduh file</span></button> --}}
 
                 <table id="table" class="table table-hover table-bordered dt-responsive p-0" style="width:100%">
                     <thead>
                         <tr class="text-center">
                             <td style="width: 10px"></td>
-                            <td>Nama Mahasiswa</td>
-                            <td>Tgl. Pengajuan</td>
+                            <td>Judul TA</td>
                             <td>Status</td>
                             <td>Aksi</td>
                             <td>Detail</td>
@@ -38,29 +37,20 @@
                         @forelse ($mahasiswa as $row)
                         <tr>
                             <td class="text-center">{{ $no++ }}</td>
-                            <td><span class="d-inline-block text-truncate konten-tabel">
-                                {{ $row->nama_mhs }}
-                            </span></td>
-                            <td class="fit-content"><span class="d-inline-block text-truncate konten-tabel" style="min-width: 100%; width: 10vw;">
-                                {{ date("d F Y", strtotime($row->created_at)) }}
+                            <td><span class="d-inline-block text-truncate konten-tabel" style=" min-width: 100%; width: 20vw;">
+                                {{ $row->judul_idn }}
                             </span></td>
                             <td class="fit-content text-capitalize text-center"><span class="d-inline-block text-truncate konten-tabel" style=" min-width: 100%; width: 10vw;">
                                 {{ $status->find($row->id_status)->status }}
                             </span></td>
                             <td class="text-center p-0">
                                 <a href="{{ route('kaprodi.detailmhs',$row->id)}}" class="btn @switch($status->find($row->id_status)->status)
-                                    @case('mendaftar')
-                                        btn-primary
-                                        @break
                                     @case('sidang')
                                         btn-dark disabled
                                         @break
-                                    @case('selesai')
-                                        btn-success
-                                        @break
                                     @default
                                         btn-warning
-                                @endswitch btn-sm px-sm-4 m-1"><i class='bx bx-spreadsheet bx-xs align-middle'></i></a>
+                                @endswitch btn-sm px-sm-4 m-1"><i class='bx bx-calendar bx-xs align-middle'></i></a>
                             </td>
                             <td></td>
                         </tr>
